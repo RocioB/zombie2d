1b19bbbdac62e530ea6f54acced8f47d71f953bb
RocioB <rociobarlo@gmail.com>
2014-12-01 18:40:21 +0100

    Botiquin trigger, añadido gas(pero no hace nada)

--- Assets/scripts/hurtSystem.cs
+++ Assets/scripts/hurtSystem.cs
@@ -6,16 +6,29 @@ public class hurtSystem : MonoBehaviour {
 	public int damage = 5;
 	public float force = 0;
 	public bool destroyOnCollision = false;
+	public bool destroyOnTrigger = false;
 	
 	private DamageSystem ds;
 	
 	void OnCollisionStay2D(Collision2D target){
-		
+		herir (target.transform);
+		if (destroyOnCollision) {
+			Destroy(gameObject);
+		}
+		}
+
+	void OnTriggerStay2D(Collider2D target){
+		herir (target.transform);
+		if (destroyOnTrigger) {
+			Destroy(gameObject);
+		}
+	}
+		void herir (Transform target) {
 		if (target.transform.tag == tagDamage) {
 			Debug.Log (target.transform.tag);
 			ds = target.transform.GetComponent<DamageSystem>();
 			if(ds){
-				Debug.Log(tagDamage + ":"+damage);
+
 				ds.hurt(damage);
 				if(force > 0){
 					var direction = target.transform.position - transform.position  ;
@@ -24,8 +37,9 @@ public class hurtSystem : MonoBehaviour {
 			}
 		}
 		
-		if (destroyOnCollision) {
-			Destroy(gameObject);
-		}
-	}
+
+	
+	
 }
+}
+	 
