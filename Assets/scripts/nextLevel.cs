906ab878d25378850cc4dac3c337ebee858d0a2e
RocioB <rociobarlo@gmail.com>
2014-12-03 18:52:59 +0100

    Teletransporte y pasar de escena

---
+++ Assets/scripts/nextLevel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextLevel : MonoBehaviour {
+
+	public string nombreNivel;
+	bool cargando=false;
+		public float tiempoEspera=3f;
+
+	void OnTriggerStay2D (Collider2D target) {
+
+				if (target.transform.tag == "Player") {
+						if (!cargando) {
+				StartCoroutine(cargaNivel());
+		}
+		}
+	}
+
+	// Use this for initialization
+
+
+
+	void OnTriggerExit2D(Collider2D target) {
+				cargando = false;
+		}
+
+
+	IEnumerator cargaNivel () {
+		cargando=true;
+		yield return new WaitForSeconds(tiempoEspera);
+		if(cargando)
+			Application.LoadLevel(nombreNivel);
+	}
+}
---
+++ Assets/scripts/puertaScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class puertaScript : MonoBehaviour {
+	public Transform destino;
+	bool delantepuerta = false;
+		bool teletransporta = false;
+
+	void OnTriggerEnter2D(Collider2D target) {
+		if (target.transform.tag == "Player")
+						delantepuerta = true;
+	}
+
+	void OnDrawGizmos () {
+				if (destino != null) {
+						Gizmos.color = Color.blue;
+						Gizmos.DrawLine (transform.position, destino.position);
+				}
+		}
+			// Use this for initialization
+			
+
+		
+
+
+
+	void Update () {
+				if (Input.GetKeyDown ("up") && delantepuerta) {
+			teletransporta = true;
+			}
+		}
+
+	void OnTriggerStay2D(Collider2D target) {
+						if (teletransporta) {
+			target.transform.position=destino.position;
+							teletransporta = false;
+							delantepuerta = false;
+						}
+					}
+
+
+	void OnTriggerExit2D(Collider2D target) {
+						if (target.transform.tag == "Player")
+							delantepuerta = false;
+
+		}
+		}
